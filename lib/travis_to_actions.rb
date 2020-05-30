41d5f5e001dcb01e112c874c3005d2f7d3364468
Nick McCurdy <nick@nickmccurdy.com>
2020-05-30 14:28:14 -0400

    Generate with bundle gem

---
+++ lib/travis_to_actions.rb
@@ -0,0 +1,6 @@
+require "travis_to_actions/version"
+
+module TravisToActions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/travis_to_actions/version.rb
@@ -0,0 +1,3 @@
+module TravisToActions
+  VERSION = "0.1.0"
+end
